fde88d73dbe34c8afa04f25c16588f51a9a0c1a6
st234pa <syoon1@stuy.edu>
2017-02-05 17:22:31 -0500

    uncommented stuff

--- db_builder.py
+++ db_builder.py
@@ -1,14 +1,21 @@
-#from pymongo import MongoClient
+#Stephanie Yoon, Angela Yu, Henry Zhang
+#SoftDev2 pd8
+#HW1 -- And Papayas
+#2017-02-06   
+
+from pymongo import MongoClient
 import csv
 
-#c=MongoClient("lisa.stuy.edu", 27017)
-#ourDB=c.MongoMadness
+c=MongoClient("lisa.stuy.edu", 27017)
+ourDB=c.MongoMadness
 
 peeps = open("peeps.csv")
 students=csv.DictReader(peeps)
 
+
 courses = open("courses.csv")
 classes=csv.DictReader(courses)
+
 
 for student in students:
     #adds grade info to student info from peeps, each code (class) is now a dict key with the value being the mark (grade)
@@ -18,5 +25,6 @@ for student in students:
     #reset cursor? so courses can be iterated again
     courses.seek(0)
     print student
+    
 
-#ourDB.students.insert_many(students)
+ourDB.students.insert_many(students)
